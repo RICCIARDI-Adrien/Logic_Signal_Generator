8304b9ad57f3657c563fd2e1838ccb406d494d4f
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2025-04-13 00:12:49 +0200

    [Software] Created the project empty layout.

---
+++ Software/Sources/Main.c
@@ -0,0 +1,13 @@
+/** @file Main.c
+ * Logic signal generator entry point and main loop.
+ * @author Adrien RICCIARDI
+ */
+#include <xc.h>
+
+//-------------------------------------------------------------------------------------------------
+// Public functions
+//-------------------------------------------------------------------------------------------------
+void main(void)
+{
+	
+}
