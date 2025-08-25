5961af26188e52c270d3bcc2e52b724eefc12f82
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2025-08-25 13:57:09 +0200

    [Software] Improved the shell by neutralizing the VT100 escape sequences and adding support for the UNIX Ctrl+C, Ctrl+D and Ctrl+U key combinations.

--- Software/Sources/Shell.c
+++ Software/Sources/Shell.c
@@ -12,6 +12,9 @@
 /** Set to 1 to enable the log messages, set to 0 to disable them. */
 #define SHELL_IS_LOGGING_ENABLED 1
 
+/** The prompt to display. */
+#define SHELL_STRING_PROMPT "> "
+
 //-------------------------------------------------------------------------------------------------
 // Public functions
 //-------------------------------------------------------------------------------------------------
@@ -30,13 +33,27 @@ void ShellReadCommandLine(char *Pointer_String_Command_Line, unsigned char Maxim
 	Maximum_Length--;
 
 	// Display the prompt
-	USBCommunicationsWriteString("\r\n> ");
+	USBCommunicationsWriteString("\r\n" SHELL_STRING_PROMPT);
 
 	while (1)
 	{
 		Character = USBCommunicationsReadCharacter();
 		switch (Character)
 		{
+			// Erase the whole line if any of the following key combination is detected
+			case 0x03: // Ctrl+C
+			case 0x04: // Ctrl+D
+			case 0x15: // Ctrl+U
+				// Return the cursor to the beginning of the line, then erase it and display the prompt again
+				USBCommunicationsWriteString("\r\033[2K" SHELL_STRING_PROMPT); // This is VT100-specific but pretty fast
+				Pointer_String_Command_Line -= Length;
+				Length = 0;
+				break;
+
+			// Discard any VT100 escape sequence, otherwise pressing some keyboard keys can mess the whole displaying
+			case 0x1B:
+				break;
+
 			case '\b':
 			case 127: // VT100 terminals send the DEL character when pressing backspace
 				// Remove the last character only if there is one
